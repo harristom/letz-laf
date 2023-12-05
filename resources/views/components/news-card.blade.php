c4932a85996f4589f6d09416087c834c6db605e9
VaniaSilvaBarbosa <barbosavania2r@gmail.com>
2023-12-05 15:30:58 +0100

    changed

--- resources/views/components/news-card.blade.php
+++ resources/views/components/news-card.blade.php
@@ -1,31 +1,29 @@
-     <div>
-        <img src="{{ $post->image_path}}" alt="">
-        <h4>{{$post->title}}</h4>
-        <p>
-           {{$post->content}}
-        </p>
-    </div>
+<div>
+    <h3>{{$post->title}}</h3>
+    <img src="{{ asset( 'images/' . $post->image_path) }}" alt="">
+    <p>{{$post->content}}</p>
+    <small>{{$post->created_at}}</small>
+</div>
 
 <style>
-div {
-    border: 1px solid white; 
-    border-radius: 25%;
-    padding: 15px; 
-    margin: 10px; 
-    width: 200px; 
-    box-shadow: 0 4px 8px rgba(87, 87, 87, 0.1); 
-}
+    div {
+        border: 1px solid white; 
+        border-radius: 25%;
+        padding: 15px; 
+        margin: 10px; 
+        width: 200px; 
+        box-shadow: 0 4px 8px rgba(87, 87, 87, 0.1); 
+    }
 
-img {
-    max-width: 100%; 
-    height: auto; 
-}
+    img {
+        max-width: 100%; 
+        height: auto; 
+    }
 
-h4 {
-    color: orange;
-    margin-top: 10px; 
-    font-size: 18px; 
-}
-
+    h4 {
+        color: orange;
+        margin-top: 10px; 
+        font-size: 18px; 
+    }
 
 </style>
