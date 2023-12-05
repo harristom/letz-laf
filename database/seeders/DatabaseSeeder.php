9ea3ef3a465b591007f663f9d3898aa636f5127a
VaniaSilvaBarbosa <barbosavania2r@gmail.com>
2023-12-05 14:04:56 +0100

    post migration, seeder and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,14 +34,22 @@ class DatabaseSeeder extends Seeder
         //Kam
 
     /*---------------POST---------------*/
-        
-    /*---------------IMAGE---------------*/
+        //fake posts
+        Post::factory(5)->create();
+        //created manually post
+        //create a new post manually
+        Post::factory()->create([
+            'title' => ' Lace Up for Luxury: Exclusive Running Events Unveiled on LëtzLaf',
+            'content' => "Luxembourg, get ready to hit the ground running! LëtzLaf is proud to present a lineup of exclusive running events set against the picturesque backdrop of our beautiful country.
+            Discover a calendar filled with meticulously curated running experiences that cater to every level of enthusiast. From scenic trail runs through Luxembourg's stunning landscapes to city marathons with a touch of grandeur, we've handpicked events that promise both fitness and luxury.
+            Immerse yourself in the thrill of the run, surrounded by the rich cultural tapestry and natural beauty that Luxembourg has to offer. Whether you are a seasoned marathoner or a casual jogger, our events are designed to provide a unique and unforgettable experience.
+            Stay ahead of the pack by checking our website for event details, registration information, and exclusive insights. Lace up your running shoes, Luxembourg a month of unparalleled running awaits you!",
+            'user_id' => $user->id,
+            'image_id' => 'luxembourg.png',
+        ]);
 
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    /*---------------EVENT---------------*/
+        //call EventSeeder.php
         $this->call([
             EventSeeder::class
         ]);
