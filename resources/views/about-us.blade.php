e0b7d454e49b5f90ac32242d546fbcbb54581688
Jaria <poba_jaria@hotmail.com>
2023-12-05 13:45:18 +0100

    about us page layout

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,60 @@
+<body>
+
+    <h3>About us</h3>
+
+    <div class="teamCardLeft AboutUsFlexbox">
+        <img class="teamPictureLeft" src="/images/charlesdeluvio-K4mSJ7kc0As-unsplash.jpg" alt="">
+
+        <div class="aboutUsDescriptionLeft aboutUsDescription">
+            <p>Kammy A</p>
+
+            <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
+        </div>
+
+    </div>
+    <div class="teamCardRight AboutUsFlexbox">
+        
+        <div class="aboutUsDescriptionRight aboutUsDescription">
+            <p>Tom HARRIS</p>
+
+            <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
+        </div>
+
+        <img class="teamPictureRight" src="" alt="">
+        
+    </div>
+    <div class="teamCard AboutUsFlexbox">
+        <img class="teamPictureLeft" src="" alt="">
+
+        <div class="aboutUsDescriptionLeft aboutUsDescription">
+            <p>Jaria POBA</p>
+
+            <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
+        </div>
+
+        
+        
+    </div>
+    <div class="teamCard AboutUsFlexbox">
+        
+        <div class="aboutUsDescriptionRight aboutUsDescription">
+            <p>Marcia SANTOS</p>
+
+            <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
+        </div>
+
+        <img class="teamPictureLeft" src="" alt=""></div>
+        
+    </div>
+    <div class="teamCard AboutUsFlexbox">
+        <img class="teamPictureLeft" src="" alt="">
+
+        <div class="aboutUsDescriptionLeft aboutUsDescription">
+            <p>Vania</p>
+
+            <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
+        </div>
+        
+    </div>
+    
+</body>
