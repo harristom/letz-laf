9f823d115a6cd7b0e380f0a1ed9eec96b1ec74e3
kella11 <kamerone@hotmail.co.uk>
2023-12-05 14:00:40 +0100

    login info

---
+++ resources/views/users/login.blade.php
@@ -0,0 +1,48 @@
+@extends('layout')
+
+@section('content')
+    <x-card>
+        <header>
+            <h2>
+                Log in
+            </h2>
+            <p>Log into your account to sign up to events!</p>
+        </header>
+
+        <form action="/login" method="POST">
+            @csrf
+
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" value="{{ old('email') }}" />
+                @error('email')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div>
+                <label for="password">
+                    Password
+                </label>
+                <input type="password" name="password" value="{{ old('password') }}" />
+                @error('password')
+                    <p>{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div>
+                <button>
+                    Sign In
+                </button>
+            </div>
+
+            <div>
+                <p>
+                    Don't have an account?
+                    <a href="/register">Register</a>
+                </p>
+            </div>
+
+        </form>
+    </x-card>
+@endSection
