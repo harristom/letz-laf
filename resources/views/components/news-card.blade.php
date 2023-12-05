661442a710a410c7f79ee1627d505c0414179fa9
marciasantos1 <marcia_santos94@hotmail.com>
2023-12-05 16:15:52 +0100

    update css

--- resources/views/components/news-card.blade.php
+++ resources/views/components/news-card.blade.php
@@ -1,19 +1,38 @@
-     <div>
-        <img src="{{ $post->image_path}}" alt="">
-        <h4>{{$post->title}}</h4>
-        <p>
-           {{$post->content}}
-        </p>
+     <div class="container">
+         <h4>{{$post->title}}</h4>
+        <div class="content one">
+            <img src="{{ $post->image_path}}" alt="">
+        </div>
+        <div class="content two">
+            <p>
+               {{$post->content}}
+            </p>
+         </div>
     </div>
 
 <style>
-div {
+.container{
+    display: flex;
+    flex-direction: row;
     border: 1px solid white; 
     border-radius: 25%;
     padding: 15px; 
     margin: 10px; 
-    width: 200px; 
+    width: 500px; 
     box-shadow: 0 4px 8px rgba(87, 87, 87, 0.1); 
+}
+
+.content{
+    display: flex;
+    flex-direction: column;
+}
+
+.one{
+    width: 30%;
+}
+
+.two{
+    width: 60%;
 }
 
 img {
