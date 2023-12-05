770bdc800caf0a0f65e92ef0aa891b7783c52be9
Tom Harris <51212431+harristom@users.noreply.github.com>
2023-12-05 10:22:07 +0100

    Add model, migration, seeder

--- database/migrations/2023_12_01_184915_create_events_table.php
+++ database/migrations/2023_12_01_184915_create_events_table.php
@@ -14,6 +14,13 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image_path')->nullable();
+            $table->dateTime('date');
+            $table->integer('distance_metres');
+            $table->float('latitude', 10, 6);
+            $table->float('longitude', 10, 6);
         });
     }
 
--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +14,91 @@ class EventSeeder extends Seeder
     public function run(): void
     {
         //
+        $runs = [
+            [
+                'name' => 'Morning Jog in Parc de la Pétrusse',
+                'description' => 'Enjoy a refreshing morning jog in Parc de la Pétrusse in Luxembourg City.',
+                'date' => '2023-12-15 08:00:00',
+                'distance_metres' => 5000,
+                'latitude' => 49.6116,
+                'longitude' => 6.1314,
+            ],
+            [
+                'name' => 'Evening Run in Parc Merl',
+                'description' => 'Experience a peaceful evening run in Parc Merl, Luxembourg City.',
+                'date' => '2023-12-20 18:30:00',
+                'distance_metres' => 8000,
+                'latitude' => 49.6107,
+                'longitude' => 6.1228,
+            ],
+            [
+                'name' => 'Trail Run in Mullerthal',
+                'description' => 'Discover the beautiful trails of Mullerthal, also known as Little Switzerland.',
+                'date' => '2024-01-10 10:00:00',
+                'distance_metres' => 10000,
+                'latitude' => 49.7785,
+                'longitude' => 6.3533,
+            ],
+            [
+                'name' => 'Afternoon Run in Parc Central',
+                'description' => 'Take an afternoon run in Parc Central, Dudelange.',
+                'date' => '2024-01-15 15:45:00',
+                'distance_metres' => 6000,
+                'latitude' => 49.4804,
+                'longitude' => 6.0837,
+            ],
+            [
+                'name' => 'City Run in Esch-sur-Alzette',
+                'description' => 'Run through the city streets of Esch-sur-Alzette.',
+                'date' => '2024-02-01 17:00:00',
+                'distance_metres' => 7000,
+                'latitude' => 49.4971,
+                'longitude' => 5.9829,
+            ],
+            [
+                'name' => 'Lakeview Run in Remerschen',
+                'description' => 'Enjoy a scenic run with a view of the lake in Remerschen.',
+                'date' => '2024-02-05 19:15:00',
+                'distance_metres' => 7500,
+                'latitude' => 49.5545,
+                'longitude' => 6.3742,
+            ],
+            [
+                'name' => 'Hilly Run in Echternach',
+                'description' => 'Challenge yourself with a hilly run in Echternach.',
+                'date' => '2024-03-01 12:30:00',
+                'distance_metres' => 9000,
+                'latitude' => 49.9746,
+                'longitude' => 6.4124,
+            ],
+            [
+                'name' => 'Sunset Run in Mondorf-les-Bains',
+                'description' => 'Experience a breathtaking sunset run in Mondorf-les-Bains.',
+                'date' => '2024-03-05 18:45:00',
+                'distance_metres' => 8500,
+                'latitude' => 49.5072,
+                'longitude' => 6.2635,
+            ],
+            [
+                'name' => 'Riverside Run in Grevenmacher',
+                'description' => 'Run along the riverside in Grevenmacher.',
+                'date' => '2024-03-15 09:30:00',
+                'distance_metres' => 7300,
+                'latitude' => 49.6719,
+                'longitude' => 6.4373,
+            ],
+            [
+                'name' => 'Forest Trail in Oesling',
+                'description' => 'Explore a forest trail in the Oesling region.',
+                'date' => '2024-04-01 14:00:00',
+                'distance_metres' => 8200,
+                'latitude' => 50.1065,
+                'longitude' => 6.0163,
+            ],
+        ];
+
+        foreach ($runs as $run) {
+            Event::create($run);
+        }
     }
 }
