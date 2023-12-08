f2ba4e7ec103d630dd2af51cc1a09bc32c1013c0
VaniaSilvaBarbosa <barbosavania2r@gmail.com>
2023-12-08 14:02:16 +0100

    trying to fix the space between the title and the icons

--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -6,7 +6,8 @@
     {{-- The photo with the date --}}
     <section class="event-card__photo--modify">
         <time class="event-card__time" datetime="{{ \Carbon\Carbon::parse($event->date)->format('d/m') }}">
-            <span class="event-card__day">{{ \Carbon\Carbon::parse($event->date)->format('d') }}</span> <span class="event-card__month">{{ \Carbon\Carbon::parse($event->date)->format('M') }}</span>
+            <span class="event-card__day">{{ \Carbon\Carbon::parse($event->date)->format('d') }}</span>
+            <span class="event-card__month">{{ \Carbon\Carbon::parse($event->date)->format('M') }}</span>
         </time>
         <img src="{{asset('images/running-is-one-of-the-best-ways-to-stay-fit-royalty-free-image-1036780592-1553033495.jpg')}}" alt="" width="640px" height="427px">
     </section>
@@ -20,8 +21,12 @@
     </section>
     {{-- The icons with the distance and participants --}}
     <aside class="event-card__distance-and-participants--details">
-        <p class="event-card__distance"><i class="fa-solid fa-person-running"></i>{{ number_format($event->distance_metres / 1000, 1) }} km</p>
-        <p class="event-card__participants"><i class="fa-solid fa-user"></i>{{ count($event->participants) }}</p>
+        <p class="event-card__distance">
+            <i class="fa-solid fa-person-running"></i>{{ number_format($event->distance_metres / 1000, 1) }} km
+        </p>
+        <p class="event-card__participants">
+            <i class="fa-solid fa-user"></i>{{ count($event->participants) }}
+        </p>
     </aside>
 </article>
 
