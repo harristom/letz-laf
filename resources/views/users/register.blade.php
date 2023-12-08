5adf925808119e1963880efb036c92604783b3bd
Tom Harris <51212431+harristom@users.noreply.github.com>
2023-12-08 10:12:48 +0100

    Fix class names, remove base styles

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,15 +1,15 @@
 @extends('layout')
 
 @section('content')
-    </div class="register-container">
+    <div class="register-container">
         <header class="register-container__header">
-            <h2 class="register-container__header--h2">Register</h2>
-            <p class="register-container__header--p">Create an account to participate!</p>
+            <h2 class="register-container__header-h2">Register</h2>
+            <p class="register-container__header-p">Create an account to participate!</p>
         </header>
-        <form action="/users" method="post"  enctype="multipart/form-data" class="register-container__form">
+        <form action="/users" method="post" enctype="multipart/form-data" class="register-container__form">
             {{-- ! added enctype to make sure the form can handle images --}}
             @csrf
-            <div class="register-container__form--div--name">
+            <div class="register-container__form-div-name">
                 <div>
                     <label for="first_name">First Name</label>
                     <input type="text" name="first_name" id="first_name" value="{{ old('first_name') }}" />
@@ -25,7 +25,7 @@
                     @enderror
                 </div>
             </div>
-            <div class="register-container__form--div--info">
+            <div class="register-container__form-div-info">
                 <div>
                     <label for="birthdate">Date of birth</label>
                     <input type="date" name="birthdate" id="birthdate" value="{{ old('birthdate') }}" />
@@ -33,16 +33,15 @@
                         <p>{{ $message }}</p>
                     @enderror
                 </div>
-                {{-- Adding section for profile picture image upload --}}
                 <div>
-                    <label for="profile_picture" >Profile picture</label>
-                    <input type="file" id="profile_picture" name="profile_picture"/>
+                    <label for="profile_picture">Profile picture</label>
+                    <input type="file" id="profile_picture" name="profile_picture" />
                     @error('profile_picture')
                         <p>{{ $message }}</p>
                     @enderror
                 </div>
             </div>
-            <div class="register-container__form--div">
+            <div class="register-container__form-div">
                 <label>Gender</label>
                 <div>
                     <label>
@@ -64,15 +63,15 @@
                     <p>{{ $message }}</p>
                 @enderror
             </div>
-            
-            <div class="register-container__form--div">
+
+            <div class="register-container__form-div">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" value="{{ old('email') }}" />
                 @error('email')
                     <p>{{ $message }}</p>
                 @enderror
             </div>
-            <div class="register-container__form--div--password">
+            <div class="register-container__form-div-password">
                 <div>
                     <label for="password">Password</label>
                     <input type="password" name="password" id="password" value="{{ old('password') }}" />
@@ -89,25 +88,26 @@
                 </div>
             </div>
 
-            <div class="register-container__form--div--btn">
+            <div class="register-container__form-div-btn">
                 <button>Sign Up</button>
             </div>
-            <div class="register-container__form--div--p">
+            <div class="register-container__form-div-p">
                 <p>
                     Already have an account?
                     <a href="/login">Login</a>
                 </p>
             </div>
-            <div class="register-container__form--div--p">
-                <p> By becoming a member, you agree to LëtzLaf's <a href="/terms-and-conditions">Terms and Conditions</a></p>
+            <div class="register-container__form-div-p">
+                <p>
+                    By becoming a member, you agree to LëtzLaf's <a
+                        href="/terms-and-conditions">Terms&nbsp;and&nbsp;Conditions</a>
+                </p>
             </div>
         </form>
-   <div>
-    
+    </div>
 @endsection
 
 <style>
-
     .register-container {
         width: 100%;
         display: flex;
@@ -121,16 +121,14 @@
         align-items: center;
     }
 
-    .register-container__header--h2{
+    .register-container__header-h2 {
         margin: 0;
-        font-family: "Inter", sans-serif;
         font-size: 40px;
         font-weight: 700;
         color: var(--primary-color);
     }
 
-    .register-container__header--p{
-        font-family: "Inter", sans-serif;
+    .register-container__header-p {
         font-size: 15px;
         max-width: 85%;
         text-align: center;
@@ -146,7 +144,7 @@
         align-items: center;
     }
 
-    .register-container__form--div {
+    .register-container__form-div {
         display: flex;
         flex-direction: column;
         gap: 20px;
@@ -154,18 +152,18 @@
         width: 500px;
     }
 
-    .register-container__form--div--name, 
-    .register-container__form--div--info,
-    .register-container__form--div--password{
+    .register-container__form-div-name,
+    .register-container__form-div-info,
+    .register-container__form-div-password {
         width: 500px;
         display: flex;
         flex-direction: row;
         gap: 20px;
     }
 
-    .register-container__form--div--name div, 
-    .register-container__form--div--info div, 
-    .register-container__form--div--password div{
+    .register-container__form-div-name div,
+    .register-container__form-div-info div,
+    .register-container__form-div-password div {
         width: 250px;
         display: flex;
         flex-direction: column;
@@ -173,69 +171,15 @@
         margin-bottom: 30px;
     }
 
-    /*BUTTONS*/
-    
-    button,
-    input[type="button"],
-    input[type="submit"],
-    .button {
-        display: inline-block;
-        padding: 10px 20px;
-        color: white;
-        border-radius: 2px;
-        text-transform: uppercase;
-        border: 2px solid var(--primary-color);
-        cursor: pointer;
-        position: relative;
-        background-color: var(--primary-color);
-        overflow: hidden;
-        z-index: 1;
-    }
-
-    button::before,
-    input[type="button"]::before,
-    input[type="submit"]::before,
-    .button::before{
-        content: "";
-        position: absolute;
-        right: 0;
-        top: 0;
-        width: 101%;
-        height: 100%;
-        background-color: white;
-        transform: translateX(-100%);
-        transition: all .3s;
-        z-index: -1;
-    }
-
-    button:hover::before,
-    input[type="button"]:hover::before,
-    input[type="submit"]:hover::before,
-    .button:hover::before{
-        transform: translateX(0);
-        color: black;
-    }
-
-    button:hover,
-    input[type="button"]:hover,
-    input[type="submit"]:hover,
-    .button:hover {
-        color: black;
-        filter: contrast(200%);
-    }
-
-    .register-container__form--div--btn{
+    .register-container__form-div-btn {
         margin: 10px auto;
         text-align: center;
         width: 150px;
     }
 
-    .register-container__form--div--p{
+    .register-container__form-div-p {
         width: 30%;
         text-align: center;
         margin: 0 auto -20px auto;
     }
-
-
-
 </style>
