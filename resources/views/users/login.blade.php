2c7c001ac5a1486531714e73905cd45f15702937
Tom Harris <51212431+harristom@users.noreply.github.com>
2023-12-08 10:02:34 +0100

    Fix class names, remove styles now in base

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -3,37 +3,37 @@
 @section('content')
     <div class="login-container">
         <header class="login-container__header">
-            <h2 class="login-container__header--h2">Log in</h2>
-            <p class="login-container__header--p">Log into your account to sign up to events!</p>
+            <h2 class="login-container__header-h2">Log in</h2>
+            <p class="login-container__header-p">Log into your account to sign up to events!</p>
         </header>
         <form action="/login" method="POST" class="login-container__form">
             @csrf
-            <div class="login-container__form--div">
+            <div class="login-container__form-div">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" value="{{ old('email') }}" />
                 @error('email')
                     <p>{{ $message }}</p>
                 @enderror
             </div>
-            <div class="login-container__form--div">
+            <div class="login-container__form-div">
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password" value="{{ old('password') }}" />
                 @error('password')
                     <p>{{ $message }}</p>
                 @enderror
             </div>
-            <div class="login-container__form--div">
-                <button class="login-container__form--div--btn">Sign In</button>
+            <div class="login-container__form-div">
+                <button class="login-container__form-div-btn">Sign In</button>
             </div>
-            <div class="login-container__form--div">
-                <p class="login-container__form--div--p">
+            <div class="login-container__form-div">
+                <p class="login-container__form-div-p">
                     Don't have an account?
                     <a href="/register">Register</a>
                 </p>
             </div>
         </form>
     </div>
-@endSection
+@endsection
 
 <style>
     
@@ -50,16 +50,14 @@
         align-items: center;
     }
 
-    .login-container__header--h2{
+    .login-container__header-h2{
         margin: 0;
-        font-family: "Inter", sans-serif;
         font-size: 40px;
         font-weight: 700;
         color: var(--primary-color);
     }
 
-    .login-container__header--p{
-        font-family: "Inter", sans-serif;
+    .login-container__header-p{
         font-size: 15px;
         max-width: 85%;
         text-align: center;
@@ -75,7 +73,7 @@
         margin-top: 20px;
     }
 
-    .login-container__form--div {
+    .login-container__form-div {
         display: flex;
         flex-direction: column;
         gap: 20px;
@@ -83,65 +81,13 @@
         width: 400px;
     }
 
-    .login-container__form--div--btn{
+    .login-container__form-div-btn{
         margin: 10px auto;
         width: 150px;
     }
 
-    .login-container__form--div--p{
+    .login-container__form-div-p{
         margin: 0 auto 0 auto;
     }
-    
-    /*BUTTONS*/
-    
-    button,
-    input[type="button"],
-    input[type="submit"],
-    .button {
-        display: inline-block;
-        padding: 10px 20px;
-        color: white;
-        border-radius: 2px;
-        text-transform: uppercase;
-        border: 2px solid var(--primary-color);
-        cursor: pointer;
-        position: relative;
-        background-color: var(--primary-color);
-        overflow: hidden;
-        z-index: 1;
-    }
-
-    button::before,
-    input[type="button"]::before,
-    input[type="submit"]::before,
-    .button::before{
-        content: "";
-        position: absolute;
-        right: 0;
-        top: 0;
-        width: 101%;
-        height: 100%;
-        background-color: white;
-        transform: translateX(-100%);
-        transition: all .3s;
-        z-index: -1;
-    }
-
-    button:hover::before,
-    input[type="button"]:hover::before,
-    input[type="submit"]:hover::before,
-    .button:hover::before{
-        transform: translateX(0);
-        color: black;
-    }
-
-    button:hover,
-    input[type="button"]:hover,
-    input[type="submit"]:hover,
-    .button:hover {
-        color: black;
-        filter: contrast(200%);
-    }
-
 
 </style>
