b9dc2c7567d6a61cf7eaeb36f313a1e8f4b06f78
kella11 <kamerone@hotmail.co.uk>
2023-12-08 13:21:08 +0100

    newx card css

--- resources/views/components/news-card.blade.php
+++ resources/views/components/news-card.blade.php
@@ -1,48 +1,56 @@
-     <div class="container">
-         <h4>{{$post->title}}</h4>
+<div class="container">
+    <h4>{{ $post->title }}</h4>
+    {{-- Check if image file exists  --}}
+    @if (file_exists(public_path($post->image_path)))
+        {{-- Image exists, show the div with the image --}}
         <div class="content one">
-            <img src="{{ $post->image_path}}" alt="">
+            <img src="{{ asset('storage/' . $post->image_path) }}" alt="">
         </div>
-        <div class="content two">
-            <p>
-               {{$post->content}}
-            </p>
-         </div>
+    @else
+        {{-- Image does not exists, hide the div --}}
+        <div style="display: none">
+        </div>
+    @endif
+    <div class="content two">
+        <p>
+            {{ $post->content }}
+        </p>
     </div>
+</div>
 
 <style>
-.container{
-    display: flex;
-    flex-direction: row;
-    border: 1px solid white; 
-    border-radius: 25%;
-    padding: 15px; 
-    margin: 10px; 
-    width: 500px; 
-    box-shadow: 0 4px 8px rgba(87, 87, 87, 0.1); 
-}
+    .container {
+        display: flex;
+        flex-direction: row;
+        border: 1px solid white;
+        border-radius: 0%;
+        padding: 15px;
+        margin: 10px;
+        width: 500px;
+        box-shadow: 0 4px 8px rgba(87, 87, 87, 0.1);
+    }
 
-.content{
-    display: flex;
-    flex-direction: column;
-}
+    .content {
+        display: ;
+        flex-direction: row;
+    }
 
-.one{
-    width: 30%;
-}
+    .one {
+        width: 30%;
+    }
 
-.two{
-    width: 60%;
-}
+    .two {
+        width: 60%;
+    }
 
     img {
-        max-width: 100%; 
-        height: auto; 
+        max-width: 100%;
+        height: auto;
     }
 
     h4 {
-        color: orange;
-        margin-top: 10px; 
-        font-size: 18px; 
+        color: #ee5c35;
+        margin-top: 10px;
+        font-size: 18px;
     }
 </style>
