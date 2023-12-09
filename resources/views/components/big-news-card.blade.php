be4a93d5ee6fd8735caf8100f0c3eb8301d306e9
Tom Harris <51212431+harristom@users.noreply.github.com>
2023-12-09 18:06:56 +0100

    Remove unused classes

--- resources/views/components/big-news-card.blade.php
+++ resources/views/components/big-news-card.blade.php
@@ -1,21 +1,21 @@
-{{--news page card--}}
+{{-- news page card --}}
 
 <div class="news-content-container">
     <div class="news-content news-div-content">
-        <h3>{{$post->title}}</h3>
+        <h3>{{ $post->title }}</h3>
         {{-- Check if image file exists  --}}
         @if (file_exists(public_path($post->image_path)))
             {{-- Image exists, show the div with the image --}}
-            <div class="news-component-container__content one">
-                <img class="news-component-container__content-img" src="{{ asset('storage/' . $post->image_path) }}" alt="">
+            <div>
+                <img src="{{ asset('storage/' . $post->image_path) }}" alt="">
             </div>
         @endif
-        <div class="news-component-container__content two">
+        <div>
             <p>{{ $post->content }}</p>
         </div>
     </div>
     <div class="news-content news-div-date">
-        <small>Created : {{$post->created_at}}</small>
+        <small>Created : {{ $post->created_at }}</small>
     </div>
 </div>
 
@@ -25,13 +25,14 @@
         width: 100%;
         display: flex;
         flex-direction: column;
-        margin: 20px; 
+        margin: 20px;
         border-radius: 10px;
-        padding: 10px 25px 25px 25px; 
-        box-shadow: 0px 0px 20px -3px rgba(0,0,0,0.2);
+        padding: 10px 25px 25px 25px;
+        box-shadow: 0px 0px 20px -3px rgba(0, 0, 0, 0.2);
         transition: transform .5s;
     }
-    .news-content-container:hover{
+
+    .news-content-container:hover {
         transform: scale(1.1);
     }
 
@@ -49,25 +50,25 @@
     .news-div p {
         margin: 0;
     }
-    
+
     .news-content h3 {
         text-align: center;
-        font-size: 30px; 
+        font-size: 30px;
     }
 
     .news-content img {
-        width: 400px; 
+        width: 400px;
         margin: auto;
-        height: auto; 
+        height: auto;
         border-radius: 5px;
         margin-right: 20px;
     }
 
     .news-content p {
-        font-size: 20px; 
+        font-size: 20px;
         text-align: center;
-        hyphens: auto; 
-	    text-align: justify
+        hyphens: auto;
+        text-align: justify
     }
 
     .news-div-date {
@@ -80,6 +81,4 @@
         text-align: left;
         padding: 20px 0 0 0;
     }
-
-
 </style>
