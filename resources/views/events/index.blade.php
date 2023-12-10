26c13111a9cd1937b4ae762e1e4684c41091d754
Tom Harris <51212431+harristom@users.noreply.github.com>
2023-12-10 18:02:58 +0100

    Add create event button

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,40 +1,68 @@
 @extends('layout')
 
 @section('content')
+    <style>
+        .events-page {
+            margin-top: 30px;
+        }
+        .events-page__list {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+            margin: 30px auto;
+            gap: 30px;
+            position: relative;
+        }
 
-<button class="past-events-btn" type="button">Previous</button>
+        .events-page__previous-btn {
+            margin: 0 auto;
+            display: block;
+        }
 
-<div class="events-page-list">
+        .events-page__add-btn-wrapper {
+            position: absolute;
+            top: 0px;
+            right: 10px;
+            z-index: 2;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: end;
+        }
 
-    @if (count($events) == 0)
-        <p>No events found</p>
-    @endif
+        .events-page__add-btn {
+            border-radius: 50%;
+            height: 3.2em;
+            width: 3.2em;
+            display: grid;
+            place-content: center;
+            position: sticky;            
+            bottom: 20px;
+            right: 20px;
+            box-shadow: 0px 0px 0px 1px var(--page-bg);
+        }
+    </style>
 
-    @foreach ($events as $event)
+    <div class="events-page">
+        <div class="events-page__btns">
+            <button class="events-page__previous-btn" type="button">Previous</button>
+        </div>
 
-        <x-event-card :event="$event" />
+        <div class="events-page__list">
+            <div class="events-page__add-btn-wrapper">
+                <a href="{{ route('events.create') }}" class="button events-page__add-btn"><i class="fa-solid fa-plus fa-xl" title="Add"></i></a>
+            </div>
 
-    @endforeach
+            @if (count($events) == 0)
+                <p>No events found</p>
+            @endif
 
-</div>
-    
+            @foreach ($events as $event)
+                <x-event-card :event="$event" />
+            @endforeach
+
+        </div>
+    </div>
 @endsection
-
-<style>
-    .events-page-list {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: center;
-        width: 80%;
-        align-items: center;
-        margin: 30px auto;
-        gap: 30px;
-      }
-
-      .past-events-btn {
-        margin: 0 auto;
-        display: block;
-      }
-
-</style>
