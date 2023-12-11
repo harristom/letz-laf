6429f5ebad405fa559095ccf4cd77bb0d104f59d
Jaria <poba_jaria@hotmail.com>
2023-12-11 13:33:33 +0100

    results search

--- resources/views/components/event-results-table.blade.php
+++ resources/views/components/event-results-table.blade.php
@@ -1,4 +1,4 @@
-<table>
+<table id="table">
     <thead>
         <tr class="manage-users-title">
             <th>Ranking</th>
@@ -9,7 +9,6 @@
         </tr>
     </thead>
     <tbody>
-
         @foreach ($event->results as $index => $result)
             <tr class="manage-users-list">
                 <td>{{ $index + 1 }}</td> {{-- Display index + 1 as the ranking --}}
@@ -20,5 +19,57 @@
                 </td>
             </tr>
         @endforeach
+        
     </tbody>
 </table>
+
+@include('partials._search')
+
+<script>
+    // Add script after the DOM has loaded
+    document.addEventListener('DOMContentLoaded', function () {
+        // Get the search input element
+        var input = document.getElementById('search-input');
+        // Get the id table element
+        var table = document.getElementById('table');
+        // Get all the rows in the table
+        var rows = table.getElementsByTagName('tr');
+
+        // Add event listener for input changes
+        input.addEventListener('input', function () {
+            var filter = input.value.toLowerCase();
+
+            // Loop through each row in the table
+            for (var i = 1; i < rows.length; i++) {
+                var row = rows[i];
+                // Get all the cells in the current row
+                var cells = row.getElementsByTagName('td');
+                // Flag indicating whether the row should be shown
+                var shouldShow = false;
+
+                // Loop through each cell in the row
+                for (var j = 0; j < cells.length; j++) {
+                    var cell = cells[j];
+                    // Check if the cell exists
+                    if (cell) {
+                        var text = cell.textContent.toLowerCase();
+                        // Check if the cell text contains the filter
+                        if (text.indexOf(filter) > -1) {
+                            shouldShow = true;
+                            break;
+                        }
+                    }
+                }
+
+                // Show or hide the row based on the filter
+                if (shouldShow) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            }
+        });
+    });
+</script>
+
+
