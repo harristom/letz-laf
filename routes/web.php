ba5a63807cbbf10f53f0a2a2c31901ab3eafda77
Tom Harris <51212431+harristom@users.noreply.github.com>
2023-12-11 11:18:02 +0100

    Revert routes for now

--- routes/web.php
+++ routes/web.php
@@ -16,94 +16,67 @@ use App\Http\Controllers\EventController;
 |
 */
 
-
-
-/*-------------------------------------------PUBLIC------------------------------------------------------*/
-//show the home page
+//link to homepage
 Route::view('/', 'index');
 
-//show all the events
-// https://laravel.com/docs/10.x/controllers#actions-handled-by-resource-controller
-Route::resource('events', EventController::class);
-// Register for an event
-Route::post('/events/{event}/register', [EventController::class, 'register'])->name('events.register');
-
-//Show register form
-Route::get('/register', [UserController::class, 'create']);
-//Add user to database
-Route::post('/users', [UserController::class, 'store']);
-
-//Show login form
-//Added a name to the route so that we can use it through middleware
-Route::get('/login', [UserController::class, 'login'])->name('login');
-//log user in
-Route::post('/login', [UserController::class, 'authenticate']);
-
-//Logout from user session
-Route::post('/logout', [UserController::class, 'logout']);
+/*----------------------POSTS----------------------*/
 
 //show all the news
 Route::get('/news', [PostController::class, 'index']);
+//create a new post
+Route::get('/news/create', [PostController::class, 'create']);
+//store a new post
+Route::post('/news', [PostController::class, 'store']);
 
+/*----------------------USERS----------------------*/
+//Show register form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+//Add user to database
+Route::post('/users', [UserController::class, 'store'])->middleware('guest');
 
-//view profile from others ???
+//Logout from user session
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+//Show login form
+//Added a name to the route so that we can use it through middleware
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+//log user in
+Route::post('/login', [UserController::class, 'authenticate'])->middleware('guest');
+
+//manage page for the Admin
+//show manage
+Route::get('/users/manage', [UserController::class, 'manage']);
+//delete user
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->where('id', '[0-9]+');
+//edit user
+Route::get('/users/{id}/edit', [UserController::class, 'editAdmin'])->where('id', '[0-9]+');
+//update user
+Route::put('/users/{id}', [UserController::class, 'updateAdmin'])->where('id', '[0-9]+');
+//add new user
+Route::get('/users/create', [UserController::class, 'createAdmin']);
+
+/*-------------------------------PROFILE------------------------------- */
 //show profile page
 Route::get('/profile/{id}', [UserController::class, 'show']);
 
+//Show update profile page
+Route::get('/profile/{id}/edit', [UserController::class, 'edit'])
+->where('id', '[0-9]+')->middleware('auth');
+//Update user info
+Route::put('/profile/{id}', [UserController::class, 'update'])
+->where('id', '[0-9]+')->middleware('auth');
 
+/*----------------------EVENTS----------------------*/
+// https://laravel.com/docs/10.x/controllers#actions-handled-by-resource-controller
+Route::resource('events', EventController::class);
 
-//show about us page
+// Register for an event
+Route::post('/events/{event}/register', [EventController::class, 'register'])
+    ->middleware('auth')
+    ->name('events.register');
+
+/*-----------------------MISC-----------------------*/
 Route::view('/about-us', 'about-us')->name('about');
+
 //link to the terms and conditions
 Route::view('/terms', 'terms')->name('terms-and-cond');
-
-/*-------------------------------------------ADMIN------------------------------------------------------*/
-Route::middleware(['auth', 'checkRole:Admin'])->group(function () {
-    // Your protected Admin routes here
-    
-    //show manage
-    Route::get('/users/manage', [UserController::class, 'manage']);
-    //delete user
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->where('id', '[0-9]+');
-    //edit user
-    Route::get('/users/{id}/edit', [UserController::class, 'editAdmin'])->where('id', '[0-9]+');
-    //update user
-    Route::put('/users/{id}', [UserController::class, 'updateAdmin'])->where('id', '[0-9]+');
-    //add new user
-    Route::get('/users/create', [UserController::class, 'createAdmin']);
-
-    //create a new post
-    Route::get('/news/create', [PostController::class, 'create']);
-    //store a new post
-    Route::post('/news', [PostController::class, 'store']);
-
-    //Show update profile page
-    Route::get('/profile/{id}/edit', [UserController::class, 'edit'])->where('id', '[0-9]+');
-    //Update user info
-    Route::put('/profile/{id}', [UserController::class, 'update'])->where('id', '[0-9]+');
-
-});
-/*-------------------------------------------ORGANISER------------------------------------------------------*/
-Route::middleware(['auth', 'checkRole:Organiser'])->group(function () {
-    // Your protected Organiser routes here
-
-    //create a new post
-    Route::get('/news/create', [PostController::class, 'create']);
-    //store a new post
-    Route::post('/news', [PostController::class, 'store']);
-    //Show update profile page
-    Route::get('/profile/{id}/edit', [UserController::class, 'edit'])->where('id', '[0-9]+');
-    //Update user info
-    Route::put('/profile/{id}', [UserController::class, 'update'])->where('id', '[0-9]+');
-});
-/*-------------------------------------------MEMBER------------------------------------------------------*/
-Route::middleware(['auth', 'checkRole:Member'])->group(function () {
-    // Your protected Member routes here
-
-    //Show update profile page
-    Route::get('/profile/{id}/edit', [UserController::class, 'edit'])->where('id', '[0-9]+');
-    //Update user info
-    Route::put('/profile/{id}', [UserController::class, 'update'])->where('id', '[0-9]+');
-
-
-});
