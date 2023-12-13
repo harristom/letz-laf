7405b110a3ed08d0e11bf56b65d7b745fc09fdd7
Tom Harris <51212431+harristom@users.noreply.github.com>
2023-12-13 22:47:55 +0100

    Fix about us styling

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,271 +1,259 @@
 @extends('layout')
 
 @section('content')
+    <h2 class="top-section__title">
+        <span class="top-section__title--black-color">About </span><span class="top-section__title--main-color">us</span>
+    </h2>
 
-<h3 class="top-section__title">
-    <p class="top-section__title--black-color" >
-        About 
-    </p>
-    <p class="top-section__title--main-color" >
-        us
-    </p>
-</h3>
+    <div class="aboutus__container">
 
-<div class="aboutus__container">
-
-    <div class="intro">
-        <div class="intro__text">
-            <h4 class="intro__text-title">Nice to meet you</h4>
-            <p class="intro__text-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptas sit mollitia officia deleniti saepe nisi eaque fuga non laudantium.</p>
-        </div>
-
-        <img class="intro__picture" src="{{asset('images/brainstormingSession.jpg')}}" alt="picture of a brain storming session">
-
-    </div>
-
-    <div class="team-card">
-        <img class="team-card__picture" src="{{asset('images/profilepicture.jpeg')}}" alt="">
-
-        <div class="team-card__description">
-            <p class="team-card__description--padding">Kammy A</p>
-            <p class="team-card__description--padding">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
-            <div>
-                <a class="team-card__linkedin-link" href="#"><i class="fa-brands fa-linkedin"></i>
-                    Get in touch
-                </a> 
-                <br>
-                <a class="team-card__portfolio-link" href="#"> <i class="fa-solid fa-laptop-code"></i>
-                    Browse my portfolio
-                </a>
+        <div class="intro">
+            <div class="intro__text">
+                <h3 class="intro__text-title">Nice to meet you</h3>
+                <p class="intro__text-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptas
+                    sit mollitia officia deleniti saepe nisi eaque fuga non laudantium.</p>
             </div>
+
+            <img class="intro__picture" src="{{ asset('images/brainstormingSession.jpg') }}"
+                alt="picture of a brain storming session">
+
         </div>
 
-    </div>
-    <div class="team-card__container--reverse">
-        <div class="team-card__description--reverse">
-            <p class="team-card__description--padding">Vania Barbosa</p>
-            <p class="team-card__description--padding">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
-            <div>
-                <a class="team-card__linkedin-link" href="#">
-                    Get in touch <i class="fa-brands fa-linkedin"></i>
-                </a> 
-                <br>
-                <a class="team-card__portfolio-link" href="#">
-                    Browse my portfolio
-                </a>
+        <div class="team-cards">
+
+            <div class="team-card">
+                <img class="team-card__picture" src="{{ asset('images/profilepicture.jpeg') }}" alt="">
+                <div class="team-card__description">
+                    <div class="team-card__text">
+                        <h3 class="team-card__title">Name</h3>
+                        <p class="team-card__bio">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
+                        <div class="team-card__socials">
+                            <a class="team-card__linkedin-link" href="#"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+                            
+                            <a class="team-card__portfolio-link" href="#"> <i class="fa-brands fa-github fa-2xl"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <img class="team-card__picture--reverse" src="{{asset('images/profilepicture.jpeg')}}" alt="">
 
-    </div>
-    <div class="team-card">
-        <img class="team-card__picture" src="{{asset('images/profilepicture.jpeg')}}" alt="">
-
-        <div class="team-card__description">
-            <p class="team-card__description--padding">Tom HARRIS</p>
-            <p class="team-card__description--padding">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
-            <div>
-                <a class="team-card__linkedin-link" href="#">
-                    Get in touch <i class="fa-brands fa-linkedin"></i>
-                </a> 
-                <br>
-                <a class="team-card__portfolio-link" href="#">
-                    Browse my portfolio
-                </a>
+            <div class="team-card">
+                <img class="team-card__picture" src="{{ asset('images/profilepicture.jpeg') }}" alt="">
+                <div class="team-card__description">
+                    <div class="team-card__text">
+                        <h3 class="team-card__title">Name</h3>
+                        <p class="team-card__bio">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
+                        <div class="team-card__socials">
+                            <a class="team-card__linkedin-link" href="#"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+                            
+                            <a class="team-card__portfolio-link" href="#"> <i class="fa-brands fa-github fa-2xl"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
 
-    </div>
-    <div class="team-card__container--reverse">
-        
-
-        <div class="team-card__description--reverse">
-            <p class="team-card__description--padding">Jaria POBA</p>
-            <p class="team-card__description--padding">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
-            <div>
-                <a class="team-card__linkedin-link" href="#">
-                    Get in touch <i class="fa-brands fa-linkedin"></i>
-                </a> 
-                <br>
-                <a class="team-card__portfolio-link" href="#">
-                    Browse my portfolio
-                </a>
+            <div class="team-card">
+                <img class="team-card__picture" src="{{ asset('images/profilepicture.jpeg') }}" alt="">
+                <div class="team-card__description">
+                    <div class="team-card__text">
+                        <h3 class="team-card__title">Name</h3>
+                        <p class="team-card__bio">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
+                        <div class="team-card__socials">
+                            <a class="team-card__linkedin-link" href="#"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+                            
+                            <a class="team-card__portfolio-link" href="#"> <i class="fa-brands fa-github fa-2xl"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <img class="team-card__picture--reverse" src="{{asset('images/profilepicture.jpeg')}}" alt="">
 
-    </div>
-    <div class="team-card">
-        <img class="team-card__picture" src="{{asset('images/profilepicture.jpeg')}}" alt="">
-
-        <div class="team-card__description">
-            <p class="team-card__description--padding">Marcia SANTOS</p>
-            <p class="team-card__description--padding">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
-            <div>
-                <a class="team-card__linkedin-link" href="#">
-                    Get in touch <i class="fa-brands fa-linkedin"></i>
-                </a> 
-                <br>
-                <a class="team-card__portfolio-link" href="#">
-                    Browse my portfolio
-                </a>
+            <div class="team-card">
+                <img class="team-card__picture" src="{{ asset('images/profilepicture.jpeg') }}" alt="">
+                <div class="team-card__description">
+                    <div class="team-card__text">
+                        <h3 class="team-card__title">Name</h3>
+                        <p class="team-card__bio">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
+                        <div class="team-card__socials">
+                            <a class="team-card__linkedin-link" href="#"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+                            
+                            <a class="team-card__portfolio-link" href="#"> <i class="fa-brands fa-github fa-2xl"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            <div class="team-card">
+                <img class="team-card__picture" src="{{ asset('images/profilepicture.jpeg') }}" alt="">
+                <div class="team-card__description">
+                    <div class="team-card__text">
+                        <h3 class="team-card__title">Name</h3>
+                        <p class="team-card__bio">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Magnam rerum itaque ad tempora reiciendis voluptatibus tempore sint totam debitis excepturi?</p>
+                        <div class="team-card__socials">
+                            <a class="team-card__linkedin-link" href="#"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+                            
+                            <a class="team-card__portfolio-link" href="#"> <i class="fa-brands fa-github fa-2xl"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-
     </div>
-</div>
-
 @endsection
 
 <style>
-
-/* ABOUT US PAGE STYLING */
-
-
-.aboutus__container{
-    margin: 0 10px;
-    padding: 0 100px;
-    max-width: 1700px;
-}
-
-.top-section__title{
-    display: flex;
-    justify-content: center;
-    text-transform: uppercase;
-}
-
-.top-section__title--main-color{
-    color: var(--primary-color);
-    padding-left: 5px;
-}
-
-.intro{
-    display: flex;
-    height: 400px;
-    width: 100%;
-    margin: 20px 10px;
-    background-color: white;
-    border-radius: 3px;  
-}
-
-.intro__text{
-    width: 50%;
-    padding: 50px; 
-}
-
-.intro__text-title{
-    color: var(--primary-color);
-    font-size: 25px;
-    text-transform: uppercase;
-}
-
-.intro__picture{
-    width: 50%;
-    flex-shrink: 0;
-}
-
-.intro__text-content{
-    font-weight: lighter;
-}
-
-.team-card{
-    display: flex;
-    width: 200px;
-    height: 250px;
-    color: var(--card-bg);
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    transition-duration: .5s;
-    cursor: pointer;
-    margin: 100px;
-}
+    /* ABOUT US PAGE STYLING */
 
 
-.team-card__container--reverse{
-    display: flex;
-    width: 200px;
-    height: 250px;
-    color: var(--card-bg);
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    transition-duration: .5s;
-    cursor: pointer;
-    margin: 100px;
-    margin-left: 70%;
-}
+    .aboutus__container {
+        margin: 0 10px;
+        padding: 0 100px;
+        max-width: 1700px;
+    }
 
-.team-card__picture {
-    position: absolute;
-    width: 250px;
-    height: 250px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    z-index: 1;
-}
+    .top-section__title {
+        display: flex;
+        justify-content: center;
+        text-transform: uppercase;
+        font-size: 2.5rem;
+        margin: 30px 5px;
+    }
 
-.team-card__picture--reverse{
-    position: absolute;
-    width: 250px;
-    height: 250px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    z-index: 1;
-}
+    .top-section__title--main-color {
+        color: var(--primary-color);
+        padding-left: 5px;
+    }
 
-.team-card__description {
-    z-index: 0;
-    width: 400px;
-    height: 300px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    background-color: var(--primary-color);
-    transition-duration: .5s;
-    font-size: 15px;
-    padding: 0 25px;
-    font-weight: lighter;
-    transform: translateX(25%);
-}
+    .intro {
+        display: flex;
+        height: 400px;
+        width: 100%;
+        margin: 20px 10px;
+        background-color: white;
+        border-radius: 3px;
+    }
 
-.team-card__description--reverse{
-    z-index: 0;
-    width: 400px;
-    height: 300px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    background-color: var(--primary-color);
-    transition-duration: .5s;
-    font-weight: lighter;
-    font-size: 15px;
-    padding: 0 25px;
-    transform: translateX(-25%);
-}
+    .intro__text {
+        width: 50%;
+        padding: 50px;
+    }
 
-.team-card:hover .team-card__description {
-    transform: translateX(110%) translateY(0%);
-    transition-duration: 0.5s;
-}
+    .intro__text-title {
+        /* color: var(--primary-color); */
+        font-size: 25px;
+        text-transform: uppercase;
+    }
 
-.team-card__container--reverse:hover .team-card__description--reverse {
-    transform: translateX(-110%) translateY(0%);
-    transition-duration: 0.5s;
-}
+    .intro__picture {
+        width: 50%;
+        flex-shrink: 0;
+        align-self: center;
+    }
 
-.team-card__portfolio-link, .team-card__linkedin-link {
-    text-decoration: none;
-    color: black;
-    font-size: 14px;
-}
+    .team-cards {
+        display: flex;
+        flex-direction: column;
+    }
 
-.team-card__portfolio-link:hover , .team-card__linkedin-link:hover {
-    color: var(--page-bg);
-}
+    .team-card {
+        display: flex;
+        height: 250px;
+        align-items: center;
+        justify-content: start;
+        margin: 100px;
+    }
 
+    .team-card:nth-child(even of .team-card) {
+        align-self: end;
+        flex-direction: row-reverse;
+        justify-content: end;
+    }
+
+
+    .team-card__picture {
+        width: 250px;
+        height: 250px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        z-index: 1;
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+    }
+
+    .team-card__description {
+        z-index: 0;
+        height: 300px;
+        background-color: var(--card-bg);
+        font-size: 15px;
+        padding: 25px;
+        transform: translateX(-85%);
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+        width: 200px;
+        transition:
+            width 300ms,
+            transform 500ms 100ms;
+    }
+
+    .team-card:nth-child(even of .team-card) .team-card__description {
+        transform: translateX(85%);
+    }
+
+    .team-card__text {
+        visibility: hidden;
+        opacity: 0;
+        margin-left: 40px;
+    }
+
+    .team-card:nth-child(even of .team-card) .team-card__text {
+        margin-left: unset;
+        margin-right: 40px;
+    }
+
+    .team-card__title {
+        font-size: 1.5rem;
+        margin-bottom: 10px;
+    }
+
+    .team-card__bio {
+        margin-bottom: 30px;
+    }
+
+    .team-card__socials {
+        display: flex;
+        gap: 3px;
+    }
+
+    .team-card__socials > *:hover {
+        filter: saturate(3);
+    }
+
+    /* Team card hover effects */
+
+    .team-card:hover .team-card__description {
+        /* transform: translateX(110%) translateY(0%); */
+        transform: translateX(-20px);
+        width: 100%;
+        transition:
+            transform 300ms,
+            width 500ms 100ms;
+    }
+
+    .team-card:nth-child(even of .team-card):hover .team-card__description {
+        /* transform: translateX(110%) translateY(0%); */
+        transform: translateX(20px);
+        width: 100%;
+        /* transition:
+            width 300ms,
+            transform 500ms 100ms; */
+    }
+
+    .team-card:hover .team-card__text {
+        visibility: visible;
+        opacity: 1;
+        transition: opacity 500ms 300ms;
+    }
 </style>
